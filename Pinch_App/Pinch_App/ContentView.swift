30999f8ddf407d13ba0aba9efcdb3b4aec055102
Nicole <lakehouse624@icloud.com>
2023-03-23 13:53:57 -0500

    new updates

--- Pinch_App/Pinch_App/ContentView.swift
+++ Pinch_App/Pinch_App/ContentView.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationView {
+            ZStack{
+                // Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow( color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                //Tap Gesture
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                        
+            }// End of ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+        }  // End of Naviation View
+        .navigationViewStyle(.stack)
+    }//End of Body View
+}//End of Content View
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Pinch_App/Pinch_App/Model/model file.swift
@@ -0,0 +1,20 @@
+//
+//  model file.swift
+//  Pinch_App
+//
+//  Created by Nicole Barnhouse on 3/23/23.
+//
+
+import SwiftUI
+
+struct model_file: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct model_file_Previews: PreviewProvider {
+    static var previews: some View {
+        model_file()
+    }
+}
