3881cfdb56d4e898fb7c9cd23acde864a01f4147
Nicole <nicolebarnhouse@gmail.com>
2023-03-23 12:13:33 -0500

    Add files

---
+++ Pinch_App/Pinch_App/Pinch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_AppApp.swift
+//  Pinch_App
+//
+//  Created by Nicole Barnhouse on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
